18b785e3ee9aaf0ff3d293fb34cd9d63000e2cb3
Joseph Taylor <joseph.taylor@xdesign.com>
2025-01-15 14:11:47 +0000

    adding variables into tests and removing redundant status variable

--- src/test/java/GetEvolutionChain.java
+++ src/test/java/GetEvolutionChain.java
@@ -1,9 +1,12 @@
 import io.restassured.RestAssured;
+import io.restassured.response.ValidatableResponse;
+import org.apache.commons.lang3.ObjectUtils;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.*;
 
 public class GetEvolutionChain {
 
@@ -14,35 +17,70 @@ public class GetEvolutionChain {
 
     @Test
     public void getEvolutionChain_withID_returns200AndValidBody() {
-        given()
-                .when()
-                .get("3")
-                .then()
-                .statusCode(200)
-                .assertThat().body("chain.species.name", equalTo("squirtle"))
-                .assertThat().body("id", equalTo(3))
-                .log().all();
+
+        int evolutionChainID = 3;
+        String pokemonName = "squirtle";
+
+        ValidatableResponse validatableResponse =
+                given()
+                        .when()
+                        .get(String.valueOf(evolutionChainID))
+                        .then()
+                        .statusCode(200)
+                        .assertThat().body("chain.species.name", equalTo(pokemonName))
+                        .assertThat().body("id", equalTo(evolutionChainID))
+                        .log().all();
+
     }
 
     @Test
     public void getEvolutionChain_withNotFoundID_returns404() {
-        given()
-                .when()
-                .get("100000000000")
-                .then()
-                .statusCode(404);
+
+        ValidatableResponse validatableResponse =
+                given()
+                        .when()
+                        .get("100000000000")
+                        .then()
+                        .statusCode(404);
+
     }
 
     @Test
-    public void getEvolutionChain_withNoId_returns200AndPaginatedResponse() {
-        given()
-                .when()
-                .get()
-                .then()
-                .statusCode(200)
-                .assertThat().body("chain.species.name", equalTo("squirtle"));
-        //Todo fix the assert
+    public void getEvolutionChain_withNoId_returns200AndDefaultPaginatedResponse() {
+
+        ValidatableResponse validatableResponse =
+                given()
+                        .when()
+                        .get()
+                        .then()
+                        .statusCode(200)
+                        .assertThat().body("count", equalTo(541))
+                        .assertThat().body("next", equalTo(baseURI + "?offset=20&limit=20"))
+                        .assertThat().body("previous", is(nullValue()))
+                        .assertThat().body("results", hasSize(20))
+                        .assertThat().body("results[0].url", equalTo(baseURI + "1/"))
+                        .assertThat().body("results[19].url", equalTo(baseURI + "20/"));
+
     }
 
+    @Test
+    public void getEvolutionChain_withNoIdAndCustomLimit_returns200() {
+
+        ValidatableResponse validatableResponse =
+                given()
+                        .queryParam("limit", 50)
+                        .queryParam("offset", 100)
+                        .when()
+                        .get()
+                        .then()
+                        .statusCode(200)
+                        .assertThat().body("count", equalTo(541))
+                        .assertThat().body("next", equalTo(baseURI + "?offset=150&limit=50"))
+                        .assertThat().body("previous", equalTo(baseURI + "?offset=50&limit=50"))
+                        .assertThat().body("results", hasSize(50))
+                        .assertThat().body("results[0].url", equalTo(baseURI + "101/"))
+                        .assertThat().body("results[49].url", equalTo(baseURI + "150/"));
+
+    }
 
 }
--- src/test/java/GetPokemon.java
+++ src/test/java/GetPokemon.java
@@ -1,6 +1,7 @@
 import static io.restassured.RestAssured.*;
 
 import io.restassured.RestAssured;
+
 import static org.hamcrest.Matchers.*;
 import static io.restassured.module.jsv.JsonSchemaValidator.*;
 
@@ -11,11 +12,11 @@ import org.testng.annotations.Test;
 
 public class GetPokemon {
 
-    int statusCode200 = 200;
-    int statusCode404 = 404;
-    int pokemonId = 7; //using int vor validation purposes but need to convert to string for Get usage
-    String pokemonSquirtle = "squirtle";
-    
+    int squirtleID = 7; //using int vor validation purposes but need to convert to string for Get usage
+    String squirtleName = "squirtle";
+    String pokemonJsonSchemaPath = "schemas/pokemon-schema.json";
+
+
     @BeforeTest
     public void setUp() {
 
@@ -28,28 +29,28 @@ public class GetPokemon {
     @Test
     public void getPokemon_conformsToJsonSchema() {
 
-        given()
-                .when()
-                .get()
-                .then()
-                .statusCode(statusCode200)
-                .assertThat()
-                .body(matchesJsonSchemaInClasspath("schemas/pokemon-schema.json"));
+        ValidatableResponse validatableResponse =
+                given()
+                        .when()
+                        .get()
+                        .then()
+                        .statusCode(200)
+                        .assertThat()
+                        .body(matchesJsonSchemaInClasspath(pokemonJsonSchemaPath));
+
     }
 
     @Test
     public void getPokemon_withId_returns200AndValidBody() {
 
-
-        
         ValidatableResponse validatableResponse =
                 given()
                         .when()
-                        .get(String.valueOf(pokemonId))
+                        .get(String.valueOf(squirtleID))
                         .then()
-                        .statusCode(statusCode200)
-                        .assertThat().body("name", equalTo(pokemonSquirtle))
-                        .assertThat().body("id", equalTo(pokemonId))
+                        .statusCode(200)
+                        .assertThat().body("name", equalTo(squirtleName))
+                        .assertThat().body("id", equalTo(squirtleID))
                         .log().all(); // logging of the whole test, can use different methods to log specific aspects
 
     }
@@ -60,11 +61,11 @@ public class GetPokemon {
         ValidatableResponse validatableResponse =
                 given()
                         .when()
-                        .get(pokemonSquirtle)
+                        .get(squirtleName)
                         .then()
-                        .statusCode(statusCode200)
-                        .assertThat().body("name", equalTo(pokemonSquirtle))
-                        .assertThat().body("id", equalTo(pokemonId));
+                        .statusCode(200)
+                        .assertThat().body("name", equalTo(squirtleName))
+                        .assertThat().body("id", equalTo(squirtleID));
 
     }
 
@@ -76,75 +77,59 @@ public class GetPokemon {
                         .when()
                         .get("10000000000000000000")
                         .then()
-                        .statusCode(statusCode404);
+                        .statusCode(404);
 
     }
 
     @Test
     public void getPokemon_withNotFoundName_returns404() {
 
-        
         ValidatableResponse validatableResponse =
                 given()
                         .when()
                         .get("TheSun")
                         .then()
-                        .statusCode(statusCode404);
+                        .statusCode(404);
 
     }
 
     @Test
-    public void getPokemon_paginated_defaultLimit() {
+    public void getPokemon_paginated_defaultLimitAndOffset() {
 
         ValidatableResponse validatableResponse =
                 given()
                         .when()
                         .get()
                         .then()
-                        .statusCode(statusCode200)
+                        .statusCode(200)
                         .assertThat().body("count", equalTo(1302))
-                        .assertThat().body("results", hasSize(20));
+                        .assertThat().body("results", hasSize(20))
+                        .assertThat().body("results[0].name", equalTo("bulbasaur"))
+                        .assertThat().body("results[19].name", equalTo("raticate"));
 
     }
 
     @Test
-    public void getPokemon_paginated_customLimit() {
+    public void getPokemon_paginated_customLimitAndOffset() {
 
         ValidatableResponse validatableResponse =
                 given()
-                        .queryParam("limit", 50)
+                        .queryParam("limit", 30) //this adds a query parameter limiting the length of the response
+                        .queryParam("offset", 30) //this further tests the params for pagination by requesting the second page
                         .when()
                         .get()
                         .then()
-                        .statusCode(statusCode200)
-                        .assertThat().body("count", equalTo(1302))
-                        .assertThat().body("results", hasSize(50));
+                        .statusCode(200)
+                        .assertThat().body("results[0].name", equalTo("nidoqueen")) //asserting the first result returned in the response array
+                        .assertThat().body("results[-1].name", equalTo("poliwag")); //[-1] array locator checks the last item in the array, would be the same as 19
 
-    }
-
-    @Test
-    public void getPokemon_paginated_customOffset() {
-
-        ValidatableResponse validatableResponse =
-                given()
-                        .queryParam("limit", 20) //this adds a query parameter limiting the length of the response
-                        .queryParam("offset", 20) //this further tests the params for pagination by requesting the second page
-                        .when()
-                        .get()
-                        .then()
-                        .statusCode(statusCode200)
-                        .assertThat().body("results[0].name", equalTo("spearow")) //asserting the first result returned in the response array
-                        .assertThat().body("results[-1].name", equalTo("wigglytuff")); //[-1] array locator checks the last item in the array, would be the same as 19
     }
 
     /*TODO
-    * For the test above you could use dynamic values instead of the hard coded values in both asserts.
-    * To do this you could send another get request within the test to get the information for the Pokemon with an ID of 21
-    * as the ID of 21 would be the 20th item in a total array.
-    *
-    * You would then store the information for pokemon 21 and compare it to what is returned in the main request.
-    * The benefits of this would be that if the data changes e.g the name of the pokemon
-    * in position 21, then this test would not still pass and continue to test the pagination functionality*/
+     * For the test above you could use dynamic values instead of the hard coded values in both asserts.
+     */
+
+    
 
 }
 
